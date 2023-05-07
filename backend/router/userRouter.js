634c8983d56906fb68a2742023196e39f0e02c26
coder-Rit <83896563+coder-Rit@users.noreply.github.com>
2023-05-08 02:18:53 +0530

    Update userRouter.js

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -6,6 +6,7 @@ const { isAuthenticated, authorizedRole } = require('../middleware/auth')
 const Router = express.Router()
 
 Router.route("/signup").post(signUp)
+Router.route("/signup/faculty").post(isAuthenticated,authorizedRole("teacher"),signUpFaculty)
 Router.route("/login").post(login)
 Router.route("/logout").get(logOut)
 Router.route("/user/isExist/:email").get(isExist)
